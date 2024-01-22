d07b7a6c22cf304aafb9e1a94858363228b45ae1
Matt Wyatt <mswyatt.td@gmail.com>
2024-01-21 20:31:38 -0700

    finished

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ const infoArrMap = infoArr.map(x => ({
     age: Number(x.age)+1
 }));
 //infoArr[3].value = "Job";
-console.log(infoArrMap)
+// console.log(infoArrMap)
 
 //occupation to job +1 to age
 
@@ -108,5 +108,29 @@ let ageSum = infoArrMap.reduce((runningSum, person) =>
 
 let averageAge = ageSum/infoArrMap.length
 
-console.log(ageSum)
-console.log(averageAge)
+// console.log(ageSum) //displays sum of ages remaining
+// console.log(averageAge) displays average of ages
+
+//-------------------------- PART 3 START
+
+function incAge(obj) {
+    obj.age += 1;
+}
+
+// console.log(infoArrMap) //before
+// incAge(infoArrMap[0])  //increment
+// console.log(infoArrMap) //+1 age
+let newObj = {}
+function copyIncAge(obj){
+    newObj = {...obj}
+    newObj.age += 1;
+}
+
+// console.log(infoArrMap[0]) //old
+// copyIncAge(infoArrMap[0]) //create and increment
+// console.log(newObj) //new
+
+//THOUGHT EXPERIMENT - I don't know. I don't have any real indea how Date works and have limited experience with setTime()
+
+//-------------------------- PART 4/5 START
+//I think I already put them in functions, if I made them without the idea of functions I think revisiting them from scratch is probably better
